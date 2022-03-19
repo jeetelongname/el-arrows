22043dbc6223bdaf70360aa2a57e107a0fa7e723
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-03-19 16:42:38 +0000

    change namespace
    reclaim those characters

--- arr.el
+++ arr.el
@@ -1,4 +1,4 @@
-;;; arrows.el --- An implementaion of some threading macro's -*- lexical-binding: t; -*-
+;;; arr.el --- An implementaion of some threading macro's -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2022 Jeetaditya Chatterjee
 ;;
@@ -8,7 +8,7 @@
 ;; Modified: March 18, 2022
 ;; Version: 0.0.1
 ;; Keywords: convenience extensions lisp
-;; Homepage: https://github.com/jeetelongname/arrows
+;; Homepage: https://github.com/jeetelongname/el-arrows
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
@@ -20,66 +20,71 @@
 ;;; Code:
 (require 'cl-lib)
 
-(defun arrows--simple-inserter (insert-fun)
+(defun arr--simple-inserter (insert-fun)
   (lambda (acc next)
     (if (listp next)
         (funcall insert-fun acc next)
       (list next acc))))
 
-(defun arrows--insert-first (arg surround)
+(defun arr--insert-first (arg surround)
   "Insert ARG into the list form SURROUND as its first argument, after the operator."
   (cl-list* (car surround)
             arg
             (cdr surround)))
 
-(defun arrows--insert-last (arg surround)
+(defun arr--insert-last (arg surround)
   "Insert ARG into the list form SURROUND as its last argument."
   (append surround (list arg)))
 
-(defmacro arrows-> (initial-form &rest forms)
+(defmacro arr-> (initial-form &rest forms)
   "Insert INITIAL-FORM as first argument into the first of FORMS.
 The result into the next, etc., before evaluation.  FORMS are treated as list designators.
 Identical in functionality to the builtin `thread-first'"
-  (cl-reduce (arrows--simple-inserter #'arrows--insert-first)
+  (cl-reduce (arr--simple-inserter #'arr--insert-first)
              forms
              :initial-value initial-form))
 
-(defmacro arrows->> (initial-form &rest forms)
+(defmacro arr->> (initial-form &rest forms)
   "Like ->, but the INITIAL-FORM are inserted as last argument in FORMS.
 Identical in functionality to the builtin `thread-last'"
-  (cl-reduce (arrows--simple-inserter #'arrows--insert-last)
+  (cl-reduce (arr--simple-inserter #'arr--insert-last)
              forms
              :initial-value initial-form))
 
-(defun arrows--diamond-inserter (insert-fun)
-  (arrows--simple-inserter (lambda (acc next)
-                     (cl-case (cl-count-if #'arrows--<>p next)
+(defun arr--diamond-inserter (insert-fun)
+  (arr--simple-inserter (lambda (acc next)
+                     (cl-case (cl-count-if #'arr--<>p next)
                        (0 (funcall insert-fun acc next))
-                       (1 (cl-substitute-if acc #'arrows--<>p next))
+                       (1 (cl-substitute-if acc #'arr--<>p next))
                        (t (let ((r (gensym "R")))
                             `(let ((,r ,acc))
-                               ,(cl-substitute-if r #'arrows--<>p next))))))))
+                               ,(cl-substitute-if r #'arr--<>p next))))))))
 
-(defun arrows--<>p (form)
+(defun arr--<>p (form)
   "Predicate identifying the placeholders in FORMs for the -<> and -<>> macros."
   (and (symbolp form)
        (string= form "<>")))
 
-(defmacro arrows-<> (initial-form &rest forms)
+(defmacro arr-<> (initial-form &rest forms)
   "Like ->, but if a form in FORMS has one or more symbols named <> as top-level element.
 Each such symbol is substituted by the primary result of the form
 accumulated so far, instead of it being inserted as first argument.  Also known
 as diamond wand."
-  (cl-reduce (arrows--diamond-inserter #'arrows--insert-first)
+  (cl-reduce (arr--diamond-inserter #'arr--insert-first)
              forms
              :initial-value initial-form))
 
-(defmacro arrows-<>> (initial-form &rest forms)
+(defmacro arr-<>> (initial-form &rest forms)
   "Like -<>, but if a form has no symbol named <>, the insertion is done at the
 end like in ->>.  Also known as diamond spear."
-  (cl-reduce (arrows--diamond-inserter #'arrows--insert-last)
+  (cl-reduce (arr--diamond-inserter #'arr--insert-last)
              forms
              :initial-value initial-form))
 
-(provide 'arrows)
-;;; arrows.el ends here
+(defmacro arr-lambda-> (&rest forms)
+  "Return a lambda for FORMS."
+  `(lambda (x)
+     (arr-> x ,@forms)))
+
+(provide 'arr)
+;;; arr.el ends here
