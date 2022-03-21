c7781fdc22f31608e83cfb7ffb109d351178ba25
Alex Birdsall <ambirdsall@gmail.com>
2022-03-20 22:22:12 -0700

    feat: add arr-?> and arr-?>>

--- arr.el
+++ arr.el
@@ -99,6 +99,34 @@ Also known as diamond spear."
              forms
              :initial-value initial-form))
 
+;;; Maybe/nil short-circuiting macros
+;;;; Internal
+(defun arr--?-inserter (insert-fun)
+  (lambda (acc next)
+    (cl-destructuring-bind (let* bindings var) acc
+      `(,let* (,@bindings
+               (,var (when ,var
+                       ,(funcall insert-fun var next))))
+         ,var))))
+
+(defun arr--expand-maybe (initial-form forms insert-fun)
+  (let ((var (gensym "maybe")))
+    (cl-reduce (arr--?-inserter (arr--simple-inserter insert-fun))
+            forms
+            :initial-value `(let* ((,var ,initial-form))
+                              ,var))))
+
+;;;###autoload
+(defmacro arr-?> (initial-form &rest forms)
+  "Like arr->, but short-circuits to nil as soon as either INITIAL-FORM or any of
+FORMS return nil.  This is like all these forms are lifted to the maybe monad."
+  (arr--expand-maybe initial-form forms #'arr--insert-first))
+
+;;;###autoload
+(defmacro arr-?>> (initial-form &rest forms)
+  "Like arr-?>, but with insertion behaviour as in arr->>."
+  (arr--expand-maybe initial-form forms #'arr--insert-last))
+
 ;;;###autoload
 (defmacro arr->* (&rest forms)
   "Like arr->, but takes its initial form as the last argument, rather than the
